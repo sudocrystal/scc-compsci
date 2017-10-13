559d98d8574f79470b78067d14a62855d8a07853
Crystal Hess <chess@shoreline.edu>
2017-10-13 12:50:05 -0700

    adds starter code for Nursery Rhyme Lab

---
+++ cs121/labs/nursery_rhymes.py
@@ -0,0 +1,23 @@
+def farm(animal,sound):
+    print("Erase this print and put your code here.")
+
+def monkeys(number):
+    print("Erase this print and put your code here.")
+
+def hickory_dickory(time):
+    print("Erase this print and put your code here.")
+
+#=====main=====
+farm("cow","moo")
+farm("duck","quack")
+#TODO: add another animal to the farm here
+
+monkeys(10)
+monkeys(9)
+#TODO: remove a monkey from the bed here
+
+hickory_dickory(1)
+hickory_dickory(2)
+#TODO: make the clock strike three here
+
+#TODO: call your new methods here (you must define them first!)
