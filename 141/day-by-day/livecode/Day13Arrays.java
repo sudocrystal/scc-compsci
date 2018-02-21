bfcbe8abf40cbda197c2c3466f0fc39c7c28a1d1
Crystal Hess <chess@shoreline.edu>
2018-02-20 19:15:49 -0800

    cleans up day 13 files saved to wrong place

--- 141/day-by-day/livecode/Day13Arrays.java
+++ 141/day-by-day/livecode/Day13Arrays.java
@@ -2,7 +2,24 @@ import java.util.*;
 
 public class Day13Arrays {
    public static void main(String[] args) {
+      arrayFundamentals();
 
+//       Random rand = new Random();
+//       int[] values =
+//       System.out.println("Count of neg = " + countNeg(values));
+
+//       makePositive(values);
+
+//       System.out.println("Min value = " + findMin(decimals));
+
+//       String[] words1 = {"apple","pear"};
+//       String[] words2 = {"apple","pear"};
+//       String[] words3 = {"orange","apple","banana"};
+//       System.out.println(Arrays.toString(words1) + " == " + Arrays.toString(words2) + "? \t\t\t\t\t" + areEqual(words1,words2));
+//       System.out.println(Arrays.toString(words2) + " == " + Arrays.toString(words3) + "? \t" + areEqual(words2,words3));
+   }
+
+   public static void arrayFundamentals() {
    }
 
    // returns a count of the negative numbers in the array
@@ -10,59 +27,7 @@ public class Day13Arrays {
       return 0;
    }
 
-   //make all negative values positive
-   public static void makePositive(int[] arr) {
-   }
-
-   // returns the smallest vlalue in the array
-   public static double findMin(double[] arr) {
-      return 0.0;
-   }
-
-   // returns true if the lists contain exactly the same items
-   // returns false otherwise
-   // **fun** can you check if they are the same, but not same order?
-   public static boolean areEqual(String[] list1, String[] list2) {
-      return false;
-   }
-}
-```
-
-## Solutions
-```java
-import java.util.*;
-
-public class Day13Arrays {
-   public static void main(String[] args) {
-      Random rand = new Random();
-      // int[] values = new int[5];
-      // for(int i = 0; i < values.length; i++)
-      //   values[i] = rand.nextInt(21) - 10;
-      // System.out.println(Arrays.toString(values));
-      // System.out.println("Count of neg = " + countNeg(values));
-      //
-      // int[] valuesCopy = makePositive(values);
-      // System.out.println(Arrays.toString(values));
-      // System.out.println(Arrays.toString(valuesCopy));
-
-      // double[] decimals = new double[10];
-      // for(int i = 0; i < decimals.length; i++)
-      //   decimals[i] = (int)(rand.nextDouble() * 1000) / 100;
-      // System.out.println("Min value = " + findMin(decimals));
-
-      String[] words1 = ["apple","pear"];
-      String[] words2 = ["apple","pear"];
-      String[] words3 = ["orange","apple","banana"];
-      System.out.println(Arrays.toString(words1) + " == " + Array.toString(words2) + "? " + areEqual(words1,words2));
-      System.out.println(Arrays.toString(words2) + " == " + Array.toString(words3) + "? " + areEqual(words2,words3));
-   }
-
-   // returns a count of the negative numbers in the array
-   public static int countNeg(int[] arr) {
-      return 0;
-   }
-
-   //make all negative values positive
+   // make all negative values positive
    public static void makePositive(int[] arr) {
    }
 
