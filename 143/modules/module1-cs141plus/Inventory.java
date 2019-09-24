2debe46f32773b597af491f2a3de2a5725bb2d22
sudocrystal <chess@shoreline.edu>
2019-09-23 20:21:07 -0700

    Removes old Inventory files

--- 143/modules/module1-cs141plus/Inventory.java
+++
@@ -1,9 +0,0 @@
-public interface Inventory {
-    public void add(String s);
-    public void subtract(String s);
-    public int get(String s);
-    public void set(String s, int count);
-    public boolean contains(String s);
-    public boolean isEmpty();
-    public int size();
-}
